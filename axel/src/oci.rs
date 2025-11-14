3a17116b52c6fb07f0dbce2a247e177e8359b7ea
wnccys <ipooo346@gmail.com>
2025-11-14 08:50:56 -0300

    feat: allow custom commands to OCI

--- axel/src/oci.rs
+++ axel/src/oci.rs
@@ -8,25 +8,31 @@ use crate::network;
 pub fn generate_oci_config(
     image_config: &network::ConfigDetails,
     container_name: &str,
+    command: Vec<String>,
 ) -> anyhow::Result<Spec> {
-    let rootfs_path = std::fs::canonicalize(format!("./woody-images/{}/rootfs", container_name))?;
+    let rootfs_path = std::fs::canonicalize(format!("./axel-images/{}/rootfs", container_name))?;
 
     let mut process_args: Vec<String> = Vec::new();
-    if let Some(entrypoint) = &image_config.entrypoint {
-        process_args.extend(entrypoint.clone());
-    }
-    if let Some(cmd) = &image_config.cmd {
-        process_args.extend(cmd.clone());
-    }
-    if process_args.is_empty() {
-        // Default to sh if no command is specified
-        process_args.push("/bin/sh".to_string());
+    if !command.is_empty() {
+        process_args.extend(command);
+    } else {
+        if let Some(entrypoint) = &image_config.entrypoint {
+            process_args.extend(entrypoint.clone());
+        }
+        if let Some(cmd) = &image_config.cmd {
+            process_args.extend(cmd.clone());
+        }
+        if process_args.is_empty() {
+            // Default to sh if no command is specified
+            process_args.push("/bin/sh".to_string());
+        }
     }
 
     let process = ProcessBuilder::default()
         .args(process_args)
         .cwd(&image_config.working_dir)
         .env(image_config.env.clone())
+        .terminal(true)
         .build()?;
 
     let root = RootBuilder::default()
