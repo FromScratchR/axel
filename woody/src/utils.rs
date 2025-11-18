6d2c770bee6e23b0c148263351fef59dcb5f8296
wnccys <ipooo346@gmail.com>
2025-11-18 11:02:17 -0300

    feat: set LinuxNamespaceType -> CloneFlags util fn

---
+++ woody/src/utils.rs
@@ -0,0 +1,17 @@
+use nix::sched::CloneFlags;
+use oci_spec::runtime::LinuxNamespaceType;
+
+pub fn spec_to_flag(s: LinuxNamespaceType) -> CloneFlags {
+    match s {
+        // "Mount" namespace is historically called CLONE_NEWNS
+        LinuxNamespaceType::Mount => CloneFlags::CLONE_NEWNS,
+        LinuxNamespaceType::Uts => CloneFlags::CLONE_NEWUTS,
+        LinuxNamespaceType::Ipc => CloneFlags::CLONE_NEWIPC,
+        LinuxNamespaceType::Pid => CloneFlags::CLONE_NEWPID,
+        LinuxNamespaceType::User => CloneFlags::CLONE_NEWUSER,
+        // CLONE_NEWTIME requires kernel 5.6+
+        LinuxNamespaceType::Time => CloneFlags::empty(),
+        LinuxNamespaceType::Cgroup => CloneFlags::CLONE_NEWCGROUP,
+        LinuxNamespaceType::Network => CloneFlags::CLONE_NEWNET,
+    }
+}
