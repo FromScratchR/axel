5b19448de7f490ab83eac6d37d632297953a5061
wnccys <ipooo346@gmail.com>
2025-11-21 16:25:01 -0300

    feat: centralize conventional container name function

--- axel/src/utils.rs
+++ axel/src/utils.rs
@@ -1,5 +1,8 @@
+/// Enable conventional "{container-name}:{tag}"
+///
 pub fn parse_image_name(image_ref: &str) -> (String, String) {
     let (image, tag) = image_ref.split_once(':').unwrap_or((image_ref, "latest"));
+
     let image_name = if image.contains('/') {
         image.to_string()
     } else {
@@ -7,4 +10,11 @@ pub fn parse_image_name(image_ref: &str) -> (String, String) {
     };
 
     (image_name.to_owned(), tag.to_owned())
+}
+
+/// ':' is invalid folder name to overlayfs;
+/// This way application uses '-' instead of ':'.
+///
+pub fn normalize_container_id<S: AsRef<str>>(s: S) -> String {
+    s.as_ref().replace(':', "-")
 }
