386029531da31ac0b84278a3c52cefe5469faf0c
wnccys <ipooo346@gmail.com>
2025-11-27 10:17:35 -0300

    feat: set container io macros

--- woody/src/macros.rs
+++ woody/src/macros.rs
@@ -1,3 +1,5 @@
+// TODO set custom macro which generate these macros
+
 macro_rules! woody {
     // Simple message
     ($msg:expr) => {
@@ -19,5 +21,29 @@ macro_rules! woody_err {
     };
 }
 
+macro_rules! container {
+    // Simple message
+    ($msg:expr) => {
+        println!("[Container] -> {}", $msg);
+    };
+    // Fmt with args
+    ($fmt:literal, $($p:expr),*) => {
+        println!("[Container] -> {}", format!($fmt, $($p),*));
+    };
+}
+
+macro_rules! container_err {
+    // Simple message
+    ($msg:expr) => {
+        println!("[Container] Error -> {}", $msg);
+    };
+    // Fmt with args
+    ($fmt:literal, $($p:expr),*) => {
+        println!("[Container] Error -> {}", format!($fmt, $($p),*));
+    };
+}
+
 pub(crate) use woody;
 pub(crate) use woody_err;
+pub(crate) use container;
+pub(crate) use container_err;
