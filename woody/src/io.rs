d5eb9d318f2ce78a2653269d28e85132d60f0b69
wnccys <ipooo346@gmail.com>
2025-12-03 11:23:25 -0300

    feat: set better Drop impl

--- woody/src/io.rs
+++ woody/src/io.rs
@@ -1,5 +1,7 @@
 use nix::sys::termios::tcsetattr;
 
+use crate::macros::woody_err;
+
 pub struct TerminalGuard {
     pub fd: i32,
     pub old_state: nix::sys::termios::Termios
@@ -8,7 +10,10 @@ pub struct TerminalGuard {
 impl Drop for TerminalGuard {
     fn drop(&mut self) {
         // Restore term old state
-        tcsetattr(self.fd, nix::sys::termios::SetArg::TCSAFLUSH, &self.old_state).expect("[Error] Could not restore old terminal state");
+        // We use if let Err(...) to consume/log the error instead of panicking
+        if let Err(e) = tcsetattr(self.fd, nix::sys::termios::SetArg::TCSAFLUSH, &self.old_state) {
+            woody_err!("Error: Could not restore old terminal state: {}", e);
+        }
     }
 }
 
