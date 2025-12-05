1240b68c509a7bc3e29e19035811d82ea709a303
wnccys <ipooo346@gmail.com>
2025-12-05 16:21:03 -0300

    feat: set basic exec working command

--- woody/src/exec.rs
+++ woody/src/exec.rs
@@ -1,19 +1,14 @@
 use anyhow::{Context, Result};
 use nix::pty::openpty;
 use nix::sched::{setns, CloneFlags};
-use nix::sys::termios::{tcgetattr, tcsetattr, LocalFlags, SetArg};
 use nix::sys::wait::waitpid;
-use nix::unistd::{close, dup2, fork, setsid, write, ForkResult};
-use nix::poll::{poll, PollFd, PollFlags};
-use nix::errno::Errno;
+use nix::unistd::{close, fork, ForkResult};
 use std::ffi::CString;
 use std::fs::{self, File};
 use std::os::unix::io::AsRawFd;
 use std::path::PathBuf;
-use nix::unistd::read;
 
-use crate::io::TerminalGuard;
-
+use crate::it;
 #[allow(unused)]
 use crate::macros::{woody, woody_err};
 
@@ -33,7 +28,6 @@ pub fn run(pids_path: PathBuf, container_id: String, command: Vec<String>) -> Re
         ("mnt", CloneFlags::CLONE_NEWNS),
         ("ipc", CloneFlags::CLONE_NEWIPC),
         ("uts", CloneFlags::CLONE_NEWUTS),
-        ("net", CloneFlags::CLONE_NEWNET),
         ("pid", CloneFlags::CLONE_NEWPID),
     ];
 
@@ -55,57 +49,12 @@ pub fn run(pids_path: PathBuf, container_id: String, command: Vec<String>) -> Re
 
             woody!("Child process spawned: {}", child);
 
-            // Handle PTY I/O (copied/adapted from main.rs)
-            let term_fd = std::io::stdin().as_raw_fd();
-            let mut termios = tcgetattr(term_fd).context("tcgetattr failed")?;
-            let _term_guard = TerminalGuard {
-                fd: term_fd,
-                old_state: termios.clone(),
-            };
-
-            termios.local_flags &= !(LocalFlags::ICANON | LocalFlags::ECHO | LocalFlags::IEXTEN | LocalFlags::ISIG);
-            tcsetattr(term_fd, SetArg::TCSAFLUSH, &termios).context("tcsetattr failed")?;
-
-            let mut fds = [
-                PollFd::new(term_fd, PollFlags::POLLIN),
-                PollFd::new(master_fd, PollFlags::POLLIN),
-            ];
-
-            loop {
-                poll(&mut fds, -1).context("poll failed")?;
-
-                if let Some(revents) = fds[0].revents() {
-                    if revents.contains(PollFlags::POLLIN) {
-                        let mut buf = [0u8; 1024];
-                        let n = read(term_fd, &mut buf).context("read from stdin failed")?;
-                        if n == 0 { break; }
-                        write(master_fd, &buf[..n]).context("write to master failed")?;
-                    }
-                }
-
-                if let Some(revents) = fds[1].revents() {
-                    if revents.contains(PollFlags::POLLHUP) {
-                        break;
-                    }
-                    if revents.contains(PollFlags::POLLIN) {
-                        let mut buf = [0u8; 1024];
-                        match read(master_fd, &mut buf) {
-                            Ok(0) => break,
-                            Ok(n) => {
-                                write(std::io::stdout().as_raw_fd(), &buf[..n])?;
-                            }
-                            Err(e) => {
-                                if e == Errno::EIO { break; }
-                            }
-                        }
-                    }
-                }
-            }
-
+            it::interactive_mode(master_fd)?;
             waitpid(child, None)?;
         }
         Ok(ForkResult::Child) => {
             close(master_fd).unwrap();
+            dbg!(&ns_fds);
 
             // Enter namespaces
             for (file, name) in ns_fds {
@@ -120,15 +69,6 @@ pub fn run(pids_path: PathBuf, container_id: String, command: Vec<String>) -> Re
                  }
             }
 
-            setsid().unwrap();
-            nix::ioctl_write_int_bad!(tiocsctty, nix::libc::TIOCSCTTY);
-            unsafe { tiocsctty(slave_fd, 1).expect("Failed to set controlling TTY"); }
-
-            dup2(slave_fd, 0).unwrap();
-            dup2(slave_fd, 1).unwrap();
-            dup2(slave_fd, 2).unwrap();
-            close(slave_fd).unwrap();
-
             // Execute command
             let cmd = if command.is_empty() {
                 "/bin/sh"
@@ -141,32 +81,25 @@ pub fn run(pids_path: PathBuf, container_id: String, command: Vec<String>) -> Re
             } else {
                 command.iter().map(|s| CString::new(s.as_str()).unwrap()).collect()
             };
-
-            // We need to fork AGAIN if we just joined PID namespace?
-            // "A process can't enter a new PID namespace and be a member of it; 
-            // setns(CLONE_NEWPID) only affects children."
-            // So the current process is still in the old PID namespace (but inside other NSs).
-            // We need to fork one more time to be *inside* the PID namespace?
-            // Yes, usually: setns -> fork -> parent waits -> child is in PID ns.
             
             match unsafe { fork() } {
                 Ok(ForkResult::Parent { child: _ }) => {
-                    // We are the intermediate process. 
-                    // We entered NSs, then forked.
-                    // We should wait for the real child.
-                    // But wait, our parent (woody main) is waiting for us.
-                    // If we wait here, we are good.
-                    // But we already did setsid/dup2? 
-                    // If we fork, the child inherits fds.
                     let mut status = 0;
                     unsafe { nix::libc::wait(&mut status) };
                     std::process::exit(0);
                 },
                 Ok(ForkResult::Child) => {
+                    it::set_slave(slave_fd);
+
+                    // Set Env
+                    unsafe { std::env::set_var("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"); }
+                    unsafe { std::env::set_var("TERM", "xterm"); }
+
                     // This child is in the new PID namespace (and others).
                     // It inherited stdio (slave_fd).
                     let c_cmd = CString::new(cmd).unwrap();
                     let _ = nix::unistd::execvp(&c_cmd, &args);
+
                     eprintln!("Failed to exec: {}", cmd);
                     std::process::exit(1);
                 }
